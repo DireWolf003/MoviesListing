303175daae5ad31bae3ab0ee9c8fd4b2f1f29430
hallgato <hallgato@LABOR210-15>
2026-09-16 09:53:30 +0200

    Add project files.

---
+++ MoviesListing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesListing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
